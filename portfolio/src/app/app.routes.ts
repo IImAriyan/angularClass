008688892049027221a39ae20248acaa7415e648
MRHidden100 <125575996+MRHidden100@users.noreply.github.com>
2024-06-15 06:23:05 -0700

    Responsive Home Page

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './_pages/home/home.component';
-import { ProjectsComponent } from './_pages/projects/projects.component';
+
 import { EducationComponent } from './_pages/education/education.component';
 import { CalculatorComponent } from './_pages/calculator/calculator.component';
 import { ContactUSComponent } from './_pages/contact-us/contact-us.component';
+import { ProjectsComponent } from './_pages/projects/projects.component';
 
 export const routes: Routes = [
     {path: '',redirectTo: 'home', pathMatch:'full'},
